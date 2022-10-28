8862239ffba1d3e6557d03b46b66b66a93d72bc8
Guilherme Araujo <guilhermelopes992@gmail.com>
2022-10-27 21:30:12 -0300

    Trabalho Final

--- GraphAlgorithms/src/mainn.java
+++ GraphAlgorithms/src/mainn.java
@@ -1,20 +1,223 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 class App {
+    public static void menu(){
+        System.out.println("\t═══════*═══════ - Bem-vindo - ═══════*════════");
+        System.out.println("Por favor, informe qual a tarefa de hoje: ");
+        System.out.println("1. Caminho Mínimo");
+        System.out.println("2. Labirinto");
+        System.out.println("3. Sair");
+    }
+
     public static void main(String[] args) throws IOException {
-        /*graphList face1 = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
+        /*graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
         graphMatrix face2 = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
-        face1.Bellman_Ford(0, 100);
-        face1.Bellman_Ford_Melhorado(0, 100);
-        face1.Dijkstra(0, 100);
+        recebaaa.Bellman_Ford(0, 100);
+        recebaaa.Bellman_Ford_Melhorado(0, 100);
+        recebaaa.Dijkstra(0, 100);
         face2.floydWarshall(0, 100);*/
 
-        graphList ny1 = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
+        /*graphList ny1 = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
         graphMatrix ny2 = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
         ny1.Bellman_Ford(0, 4);
         ny1.Bellman_Ford_Melhorado(0, 4);
         ny1.Dijkstra(0, 4);
-        ny2.floydWarshall(0, 4);
+        ny2.floydWarshall(0, 4);*/
+
+
+            int opcao;
+            int arquivo;
+            int origem;
+            int destino;
+            int opcao2;
+            Scanner entrada = new Scanner(System.in);
+            
+            do{
+                menu();
+                opcao = entrada.nextInt();
+                
+                switch(opcao){
+                case 1:
+                    System.out.println("\nSelecione o arquivo que você deseja usar: ");
+                    System.out.println("1 - facebook_combined.txt");
+                    System.out.println("2 - rg300_4730.txt");
+                    System.out.println("3 - rome99c.txt");
+                    System.out.println("4 - toy.txt");
+                    System.out.println("5 - USA-road-dt.DC.txt");
+                    System.out.println("6 - USA-road-dt.NY.txt");
+                    arquivo = entrada.nextInt();
+
+                    System.out.println("\nInforme o nó de origem: ");
+                    origem = entrada.nextInt();
+                    System.out.println("Agora, informe o nó de destino: ");
+                    destino= entrada.nextInt();
+
+                        if(arquivo == 1) {
+                            System.out.println("\nEstamos quase lá! \nSelecione o algoritmo que você deseja usar: ");
+                            System.out.println("1 - Bellman-Ford");
+                            System.out.println("2 - Bellman-Ford Melhorado");
+                            System.out.println("3 - Dijkstra");
+                            System.out.println("4 - Floyd Warshall");
+                            opcao2 = entrada.nextInt();
+
+                            if(opcao2 == 1) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford(origem, destino);
+                            } else if(opcao2 == 2) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford_Melhorado(origem, destino);
+                            } else if(opcao2 == 3) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Dijkstra(origem, destino);
+                            } else if(opcao2 == 4) {
+                                graphMatrix recebaaa = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\facebook_combined.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.floydWarshall(origem, destino);
+                            }
+
+                        } else if(arquivo == 2) {
+                            System.out.println("\nEstamos quase lá! \nSelecione o algoritmo que você deseja usar: ");
+                            System.out.println("1 - Bellman-Ford");
+                            System.out.println("2 - Bellman-Ford Melhorado");
+                            System.out.println("3 - Dijkstra");
+                            System.out.println("4 - Floyd Warshall");
+                            opcao2 = entrada.nextInt();
+
+                            if(opcao2 == 1) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rg300_4730.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford(origem, destino);
+                            } else if(opcao2 == 2) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rg300_4730.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford_Melhorado(origem, destino);
+                            } else if(opcao2 == 3) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rg300_4730.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Dijkstra(origem, destino);
+                            } else if(opcao2 == 4) {
+                                graphMatrix recebaaa = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rg300_4730.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.floydWarshall(origem, destino);
+                            }
+                        } else if(arquivo == 3) {
+                            System.out.println("\nEstamos quase lá! \nSelecione o algoritmo que você deseja usar: ");
+                            System.out.println("1 - Bellman-Ford");
+                            System.out.println("2 - Bellman-Ford Melhorado");
+                            System.out.println("3 - Dijkstra");
+                            System.out.println("4 - Floyd Warshall");
+                            opcao2 = entrada.nextInt();
+
+                            if(opcao2 == 1) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rome99c.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford(origem, destino);
+                            } else if(opcao2 == 2) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rome99c.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford_Melhorado(origem, destino);
+                            } else if(opcao2 == 3) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rome99c.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Dijkstra(origem, destino);
+                            } else if(opcao2 == 4) {
+                                graphMatrix recebaaa = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\rome99c.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.floydWarshall(origem, destino);
+                            }
+                        } else if(arquivo == 4) {
+                            System.out.println("\nEstamos quase lá! \nSelecione o algoritmo que você deseja usar: ");
+                            System.out.println("1 - Bellman-Ford");
+                            System.out.println("2 - Bellman-Ford Melhorado");
+                            System.out.println("3 - Dijkstra");
+                            System.out.println("4 - Floyd Warshall");
+                            opcao2 = entrada.nextInt();
+
+                            if(opcao2 == 1) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford(origem, destino);
+                            } else if(opcao2 == 2) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford_Melhorado(origem, destino);
+                            } else if(opcao2 == 3) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Dijkstra(origem, destino);
+                            } else if(opcao2 == 4) {
+                                graphMatrix recebaaa = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\toy.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.floydWarshall(origem, destino);
+                            }
+                        } else if(arquivo == 5) {
+                            System.out.println("\nEstamos quase lá! \nSelecione o algoritmo que você deseja usar: ");
+                            System.out.println("1 - Bellman-Ford");
+                            System.out.println("2 - Bellman-Ford Melhorado");
+                            System.out.println("3 - Dijkstra");
+                            System.out.println("4 - Floyd Warshall");
+                            opcao2 = entrada.nextInt();
+
+                            if(opcao2 == 1) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.DC.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford(origem, destino);
+                            } else if(opcao2 == 2) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.DC.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford_Melhorado(origem, destino);
+                            } else if(opcao2 == 3) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.DC.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Dijkstra(origem, destino);
+                            } else if(opcao2 == 4) {
+                                graphMatrix recebaaa = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.DC.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.floydWarshall(origem, destino);
+                            }
+                        } else if(arquivo == 6) {
+                            System.out.println("\nEstamos quase lá! \nSelecione o algoritmo que você deseja usar: ");
+                            System.out.println("1 - Bellman-Ford");
+                            System.out.println("2 - Bellman-Ford Melhorado");
+                            System.out.println("3 - Dijkstra");
+                            System.out.println("4 - Floyd Warshall");
+                            opcao2 = entrada.nextInt();
+
+                            if(opcao2 == 1) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.NY.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford(origem, destino);
+                            } else if(opcao2 == 2) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.NY.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Bellman_Ford_Melhorado(origem, destino);
+                            } else if(opcao2 == 3) {
+                                graphList recebaaa = new graphList("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.NY.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.Dijkstra(origem, destino);
+                            } else if(opcao2 == 4) {
+                                graphMatrix recebaaa = new graphMatrix("C:\\Users\\guilh\\OneDrive\\Documentos\\Faculdade\\Períodos\\3º Período\\AEDS III\\Codes\\GraphAlgorithms\\cm\\USA-road-dt.NY.txt.txt");
+                                System.out.println("\nAqui está o seu resultado:\n");
+                                recebaaa.floydWarshall(origem, destino);
+                            }
+                        }
+
+                    break;
+                    
+                case 2:
+                    
+                    break;
+                
+                default:
+                    System.out.println("Obrigado! (Ajuda nós, George!!! :) )");
+                }
+            } while(opcao != 3);
+
+            entrada.close();
 
         // System.out.println(g.isOriented());
         // System.out.println(g1.prim());
